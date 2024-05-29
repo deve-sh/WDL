8040a1406c1b0853d21792b97280df6fb6452605
Devesh Kumar <devesh2027@gmail.com>
2024-05-29 15:44:30 +0530

    feat: Setup Libs via TS Config + Setup Builder UI

---
+++ builder/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			libs: path.resolve(__dirname, "../libs"),
+		},
+	},
+	optimizeDeps: {
+		include: ["libs"],
+	},
+});
