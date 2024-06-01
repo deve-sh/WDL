c2b679213ecab0df5042122909daf615c41a8efa
Devesh Kumar <devesh2027@gmail.com>
2024-06-01 15:41:38 +0530

    ci(Tests): Add tests for Redirect Step

---
+++ e2e/unit-and-integration-tests/steps/redirect-step.test.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it } from "@jest/globals";
+import Workflow from "../../../lib/api/Workflow";
+import workflowTemplate from "../mocks/sampleWorkflow";
+
+describe("Redirection/OAuth-Flow Step tests", () => {
+	it("should resolve interpolated values correctly & redirect client correctly to the final url automatically", async () => {
+		// @ts-expect-error Filling in a window mock for the server environment without an additional library
+		global.window = { location: { href: "" } };
+
+		const oAuthURL = "http://localhost:8080/redirectToKyc/";
+		const oAuthClientId = "abcdef";
+
+		const workflow = new Workflow(workflowTemplate, {
+			environmentContext: {
+				variables: { oAuthClientId: oAuthClientId },
+			},
+		}).loadCurrentState({
+			currentStep: "startOAuthStep",
+			metadata: {
+				general: {},
+				redirectURLComputer: { finalURL: oAuthURL },
+			},
+		});
+
+		await workflow.processCurrentStep();
+
+		expect(global.window.location.href.toString()).toBe(
+			oAuthURL + "?clientId=" + oAuthClientId
+		);
+	});
+
+	it("should require a resolver for the action to be processed on the server", async () => {
+		const workflow = new Workflow(workflowTemplate).loadCurrentState({
+			currentStep: "startOAuthStep",
+			metadata: { general: {} },
+		});
+
+		try {
+			await workflow.processCurrentStep();
+		} catch (error) {
+			expect(error.message).toMatch("does not have a valid action or resolver");
+		}
+	});
+
+	it("should invoke resolver for the redirect step to be processed on the server", async () => {
+		const workflow = new Workflow(workflowTemplate).loadCurrentState({
+			currentStep: "startOAuthStep",
+			metadata: { general: {} },
+		});
+
+		let resolverCalled = false;
+
+		workflow.registerResolver("startOAuthStep", () => {
+			resolverCalled = true;
+		});
+
+		await workflow.processCurrentStep();
+		expect(resolverCalled).toBe(true);
+	});
+});
